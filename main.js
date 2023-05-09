970f6a9332e8bc40e4c6eee90ae041094bb24850
Jair130 <100359718+Jair130@users.noreply.github.com>
2023-05-09 20:29:09 +0100

    inteface

--- main.js
+++ main.js
@@ -1,16 +1,82 @@
 'use strict';
 
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
-var mainWindow = null;
+console.log("main process working");
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
-});
+console.log('main.js');
 
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({ width: 650, height: 510 });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.on('closed', function () { mainWindow = null; });
-});
+
+
+const electron =  require("electron");
+
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+
+const url = require("url");
+
+
+
+let win;
+
+
+
+function createWindow(){
+
+    win = new BrowserWindow({
+
+        webPreferences:{
+
+            nodeIntegration:true,
+
+            contextIsolation:false,
+
+        }});
+ win.loadURL(url.format({
+
+        pathname: path.join(__dirname, 'index.html'),
+
+        protocol: 'file',
+
+        slashes: true
+
+    }));
+
+
+
+    
+
+    
+
+
+
+    win.on('closed', ()=>{
+
+        win = null;
+
+    })
+
+}
+
+
+
+app.on('ready', createWindow);
+
+
+
+app.on('window-all-closed', () =>{
+
+    if(process.platform !== 'darwin'){
+
+        app.quit()
+
+    }});
+
+app.on('activate', ()=> {
+
+    if(win === null){
+
+        createWindow()
+
+    }});
